3c750d0df50f7c188d3fcb82e78ce7348073e23a
rantnar <kulinski90@gmail.com>
2024-10-22 15:36:52 +0200

    Add Arkadia Soundsense integration

---
+++ arkadia_soundsense.lua
@@ -0,0 +1,59 @@
+arkadia_soundsense = arkadia_soundsense or {}
+
+arkadia_soundsense.soundtable = {
+    ["walka"] = {
+        ["zadane"] = {
+            ["1"] = "sounds/walka_zadane_1.mp3",
+            ["2"] = "sounds/walka_zadane_2.mp3",
+            ["3"] = "sounds/walka_zadane_3.mp3",
+        },
+        ["otrzymane"] = {
+            ["1"] = "sounds/walka_otrzymane_1.mp3",
+            ["2"] = "sounds/walka_otrzymane_2.mp3",
+            ["3"] = "sounds/walka_otrzymane_3.mp3",
+        },
+        ["uniki"] = {
+            ["1"] = "sounds/walka_uniki_1.mp3",
+            ["2"] = "sounds/walka_uniki_2.mp3",
+            ["3"] = "sounds/walka_uniki_3.mp3",
+        },
+        ["parowanie"] = {
+            ["1"] = "sounds/walka_parowanie_1.mp3",
+            ["2"] = "sounds/walka_parowanie_2.mp3",
+            ["3"] = "sounds/walka_parowanie_3.mp3",
+        }
+    },
+    ["env"] = {
+        ["deszcz"] = "sounds/deszcz.mp3",
+        ["burza"] = "sounds/burza.mp3",
+        ["wiatr"] = "sounds/wiatr.mp3",
+    }
+}
+
+local sound_map = {
+    ["ledwo muskasz"] = arkadia_soundsense.soundtable["walka"]["zadane"]["1"],
+    ["uuu wieje wiatr"] = arkadia_soundsense.soundtable["env"]["wiatr"]
+}
+
+-- Function to play sound
+function arkadia_soundsense:play_sound(sound)
+    if sound == "" or sound == nil then
+        return
+    end
+    playSoundFile(sound)
+end
+
+-- Function to register sound handlers
+function arkadia_soundsense:register_sound_handler(pattern, sound)
+    registerAnonymousEventHandler("gmcp.gmcp_msgs.decoded", function()
+        local message = ansi2string(gmcp.gmcp_msgs.decoded)
+        if message:find(pattern) then
+            arkadia_soundsense:play_sound(sound)
+        end
+    end)
+end
+
+-- Register handlers for each pattern in the sound_map
+for pattern, sound in pairs(sound_map) do
+    arkadia_soundsense:register_sound_handler(pattern, sound)
+end
